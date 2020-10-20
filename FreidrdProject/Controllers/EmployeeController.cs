099b353e19c13a4005f4d380ee0770cf376d1714
Bob Freid <freidrd@mail.uc.edu>
2020-10-20 17:31:38 -0400

    Create a variable that acts as a type RFreidConstDbContext and initalize it in EmployeeController.cs

--- FreidrdProject/Controllers/EmployeeController.cs
+++ FreidrdProject/Controllers/EmployeeController.cs
@@ -2,12 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FreidrdProject.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FreidrdProject.Controllers
 {
 	public class EmployeeController : Controller
 	{
+		private readonly RFreidConstDbContext _db;
+
+		public EmployeeController(RFreidConstDbContext db)
+		{
+			_db = db;
+		}
 		public IActionResult Index()
 		{
 			return View();
