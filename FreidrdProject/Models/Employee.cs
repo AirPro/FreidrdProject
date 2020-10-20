83c9d017454d0ad25dce802cff1bb0acd3e42017
Bob Freid <freidrd@mail.uc.edu>
2020-10-20 15:38:05 -0400

    Completed creating and establishing the database and the employee page

---
+++ FreidrdProject/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FreidrdProject.Models
+{
+	public enum Department
+	{
+		Tradesmen, Management, Admin, Marketing, HR
+	}
+	public class Employee
+	{
+		[Key]
+		public int EmployeeID { get; set; }
+
+		[Required]
+		public string FirstName { get; set; }
+
+		[Required]
+		public string LastName { get; set; }
+
+		[Required]
+		public string Position { get; set; }
+
+		[Required]
+		public Department Department { get; set; }
+
+		public double Salary { get; set; }
+
+		public DateTime DateJoined { get; set; }
+		public DateTime LastUpdated { get; set; }
+	}
+}
---
+++ FreidrdProject/Models/RFreidConstDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace FreidrdProject.Models
+{
+	public class RFreidConstDbContext : DbContext
+	{
+		public RFreidConstDbContext(DbContextOptions<RFreidConstDbContext> options) : base(options)
+		{
+
+		}
+
+		public DbSet<Employee> Employees { get; set; }
+	}
+}
