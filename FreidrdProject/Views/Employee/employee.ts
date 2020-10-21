ba04cb6126f15bb6dd9440b241e258c227b6ea04
Bob Freid <freidrd@mail.uc.edu>
2020-10-21 09:39:46 -0400

    Corrected error in _TableData.Appended the employee.ts file to define custom url for controller routing. create custom method named init and initTable.

--- FreidrdProject/Views/Employee/employee.ts
+++ FreidrdProject/Views/Employee/employee.ts
@@ -1,5 +1,31 @@
 ﻿class Employees {
 
+    private urlGetData = "/employee/table-data-view";
+
+    constructor() {
+        this.init();
+    }
+
+    private init() {
+        try {
+            this.initTable();
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    private initTable() {
+        try {
+            Util.request(this.urlGetData, 'GET', 'html', (response) => {
+                $('#employees_list tbody').empty();
+                $('#employees_list tbody').append(response);
+            }, function () {
+                console.error('Failed to get data. Please try again');
+            });
+        } catch (e) {
+            console.error(e);
+        }
+    }
 }
 
 $(document).ready(function () {
