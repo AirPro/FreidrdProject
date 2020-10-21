ff69bfbf3aec17c6f88c7dfd3e0b95e9e96a615b
Bob Freid <freidrd@mail.uc.edu>
2020-10-21 08:56:05 -0400

    Completed the util.js file for handling requests coming from the controller

---
+++ FreidrdProject/wwwroot/js/util.js
@@ -0,0 +1,46 @@
+﻿var Util = (function (scope) {
+
+    scope.request = function (url, type, dataType, successCallback, failureCallback, data) {
+        try {
+            if (data === undefined || data === null) {
+                data = {};
+            }
+            $.ajax(
+                {
+                    type: type, //GET or POST
+                    dataType: dataType, //html of json
+                    processData: true,
+                    data: data,
+                    url: url,
+                    statusCode: processResponse(successCallback, failureCallback),
+                    complete: function () {
+
+                    }
+                });
+        } catch (e) {
+            console.error(e)
+
+        }
+    }
+
+    function processResponse(successCalback, failureCallback) {
+        return {
+            200: successCalback,
+            401: handleUnauthorizedError,
+            403: handleForbiddenError,
+            404: failureCallback,
+            500: failureCallback
+        };
+    }
+
+    function handleUnauthorizedError() {
+        location.reload();
+    }
+
+    function handleForbiddenError() {
+        $.notify('No permission to access.');
+    }
+
+    return scope;
+
+})({});
